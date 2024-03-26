0a3938594d65ba779a08cc314cf0ea17aa7dfddb
vvtrnv <swatik07@yandex.ru>
2024-03-27 00:11:04 +0700

    fix base service interface

--- libs/backend/src/lib/base/interfaces/services/base.service.interface.ts
+++ libs/backend/src/lib/base/interfaces/services/base.service.interface.ts
@@ -4,8 +4,8 @@ import { BaseCreateDto } from '../../classes/dto/base.create.dto';
 import { BaseUpdateDto } from '../../classes/dto/base.update.dto';
 
 export interface IBaseService<Entity = any> {
-  findAll(dto?: BaseSearchDto): Entity[];
-  findById(id: UUID): Entity | null;
-  create(dto: BaseCreateDto): Entity;
-  update(dto: BaseUpdateDto): void;
+  findAll(dto?: BaseSearchDto): Promise<Entity[]>;
+  findById(id: UUID): Promise<Entity> | Promise<null>;
+  create(dto: BaseCreateDto): Promise<Entity>;
+  update(id: string, dto: BaseUpdateDto): Promise<void>;
 }
