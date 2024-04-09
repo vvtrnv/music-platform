07f42115c8a781da721d8501d770046b609f71fb
vvtrnv <swatik07@yandex.ru>
2024-04-09 23:16:34 +0900

    add swagger on /api/v1/docs

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,12 +2,26 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = 'api/v1';
+  const swaggerPrefix = 'docs';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  if (process.env.SWAGGER) {
+    const config = new DocumentBuilder()
+    .setTitle('music-platform')
+    .setDescription('API')
+    .setVersion('1.0')
+    .addTag('track')
+    .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(`${globalPrefix}/${swaggerPrefix}`, app, document);
+  }
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
