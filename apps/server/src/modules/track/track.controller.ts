7db54ba00bd886d50c640bbe0b2692eb85694ee9
vvtrnv <swatik07@yandex.ru>
2024-04-09 23:41:43 +0900

    add methods to TrackController

--- apps/server/src/modules/track/track.controller.ts
+++ apps/server/src/modules/track/track.controller.ts
@@ -1,5 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 import { TrackService } from './track.service';
+import { TrackSearchDto } from './dto/track.search.dto';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
+import { TrackCreateDto } from './dto/track.create.dto';
+import { TrackUpdateDto } from './dto/track.update.dto';
 
 @Controller('track')
 export class TrackController {
@@ -7,4 +11,29 @@ export class TrackController {
     protected trackService: TrackService,
   ) {}
 
+  @Get()
+  @ApiOperation({})
+  public findAll(@Query() dto: TrackSearchDto) {
+    return this.trackService.findAll(dto);
+  }
+
+  @Get(':id')
+  @ApiOperation({})
+  @ApiParam({ name: 'id' })
+  public findById(@Param() id: string) {
+    return this.trackService.findById(id);
+  }
+
+  @Post()
+  @ApiOperation({})
+  public create(@Body() dto: TrackCreateDto) {
+    return this.trackService.create(dto);
+  }
+
+  @Patch(':id')
+  @ApiOperation({})
+  @ApiParam({ name: 'id' })
+  public update(@Param() id: string, @Body() dto: TrackUpdateDto) {
+    return this.trackService.update(id, dto);
+  }
 }
